7809c4951b6a5c92ebf30249d74b29c5fb3a4a86
EdselBNicolas <ebnicolas.halcyondigital@gmail.com>
2018-12-13 07:18:36 +0800

    get by specific id added

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,13 +1,13 @@
 import {
-    Get,
-    Post,
-    Body,
-    Controller,
-    UsePipes,
-    Delete,
-    Param,
-    Put,
-  } from '@nestjs/common';
+  Get,
+  Post,
+  Body,
+  Controller,
+  UsePipes,
+  Delete,
+  Param,
+  Put,
+} from '@nestjs/common';
 import { CreateItemDto } from './create-item.dto';
 import { ItemsService } from './items.service';
 import { Item } from './items.interface';
@@ -16,26 +16,31 @@ import { ValidationPipe } from '../common/validation.pipe';
 @Controller('items')
 export class ItemsController {
 
-  constructor(private readonly itemsService: ItemsService) {}
+  constructor(private readonly itemsService: ItemsService) { }
 
   @Get()
-    async findAll(): Promise<Item[]> {
-      return this.itemsService.findAll();
-    }
+  async findAll(): Promise<Item[]> {
+    return this.itemsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id): Promise<Item[]> {
+    return this.itemsService.findOne(id);
+  }
 
   @Post()
-    @UsePipes(new ValidationPipe())
-    async create(@Body() createItemDto: CreateItemDto) {
-      this.itemsService.create(createItemDto);
-    }
+  @UsePipes(new ValidationPipe())
+  async create(@Body() createItemDto: CreateItemDto) {
+    this.itemsService.create(createItemDto);
+  }
 
   @Delete(':id')
-    async deleteItem(@Param('id') id) {
-      this.itemsService.delete(id);
-    }
+  async deleteItem(@Param('id') id) {
+    this.itemsService.delete(id);
+  }
 
   @Put(':id')
-    async updateItem(@Param('id') id, @Body() createItemDto: CreateItemDto) {
-      this.itemsService.updateItem(id, createItemDto);
-    }
+  async updateItem(@Param('id') id, @Body() createItemDto: CreateItemDto) {
+    this.itemsService.updateItem(id, createItemDto);
+  }
 }
