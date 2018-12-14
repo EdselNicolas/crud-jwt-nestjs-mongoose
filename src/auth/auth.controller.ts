a36393a2f1efc6281fd11c404e53e1d0757fa281
EdselBNicolas <ebnicolas.halcyondigital@gmail.com>
2018-12-14 09:06:30 +0800

    token based authentication added

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,34 @@
+import { LoginUserDto } from './../users/login-user.dto';
+import { JwtPayload } from './jwt-payload.interface';
+import { Controller, Get, UseGuards, Post, Body, Response, Inject, HttpStatus } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { UsersService } from 'src/users/users.service';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService, private readonly usersService: UsersService) { }
+
+    @Post('login')
+    async login(@Body() payload: JwtPayload) {
+        const user = await this.usersService.findOne(payload);
+        if (user) {
+            if (await this.usersService.compareHash(payload.password, user['password'])) {
+                return await this.authService.createToken(payload);
+            } else {
+                return user;
+            }
+        }
+    }
+
+    @Post('token')
+    async createToken(@Body() payload: JwtPayload): Promise<any> {
+        return await this.authService.createToken(payload);
+    }
+
+    @Get('data')
+    @UseGuards(AuthGuard())
+    findAll() {
+        // This route is restricted by AuthGuard
+    }
+}
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+import { UsersService } from 'src/users/users.service';
+import { UsersModule } from 'src/users/users.module';
+import { UserServiceProvider } from 'src/users/users.provider';
+
+@Module({
+    imports: [
+        PassportModule.register({ defaultStrategy: 'jwt' }),
+        JwtModule.register({
+            secretOrPrivateKey: 'secretKey',
+            signOptions: {
+                expiresIn: 3600,
+            },
+        }),
+        UsersModule,
+    ],
+    controllers: [AuthController],
+    providers: [
+        AuthService,
+        JwtStrategy,
+        // ...UserServiceProvider,
+    ],
+})
+export class AuthModule { }
