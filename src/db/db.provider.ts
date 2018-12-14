01f3f445f8c25b31dfa714515831b2d293531935
EdselBNicolas <ebnicolas.halcyondigital@gmail.com>
2018-12-14 09:07:32 +0800

    multiple connection on db

--- src/db/db.provider.ts
+++ src/db/db.provider.ts
@@ -4,6 +4,6 @@ export const databaseProviders = [
   {
     provide: 'DbConnectionToken',
     useFactory: async (): Promise<typeof mongoose> =>
-      await mongoose.connect('mongodb://localhost/nest'),
+      await mongoose.createConnection('mongodb://localhost/nest'),
   },
 ];
